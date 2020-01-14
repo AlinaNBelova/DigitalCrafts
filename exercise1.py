245009d0cdeb5c84c870dead7d695b26a5cb8328
Alina Belova <belovaalni@gmail.com>
2020-01-14 16:38:57 -0600

    wip

---
+++ exercise1.py
@@ -0,0 +1,34 @@
+# #1
+# name = input('What is your name?  ')
+# print(f'Hello{name}')
+
+# #2
+# name = input ('WHAT IS YOUR NAME?')
+# name_caps = name.upper()
+# print (f'HELLO , {name_caps}')
+# print (f'YOUR NAME HAS {len(name)} LETTERS IN IT! AWESOME!')
+
+
+# #3 
+# name = input('What is your name?')
+# subject = input('What is subject?')
+# print( f'{name}\'s favorite subject in school is {subject}')
+
+#4
+day = int(input('Day (0-6)? '))
+if day ==0:
+    print("Sunday")
+if day ==1:
+   print("Monday")
+if day ==2:
+    print("Tuesday")
+if day ==3:
+    print("Wednesday")
+if day ==4:
+    print("Thursday")
+if day ==5:
+    print("Friday")
+if day ==6:
+    print("Saturday")
+
+
