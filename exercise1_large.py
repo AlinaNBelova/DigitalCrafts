412c28d41062c793f57e5628229fdad6da3ac6d2
Alina Belova <belovaalni@gmail.com>
2020-01-14 18:47:12 -0600

    wip

---
+++ exercise1_large.py
@@ -0,0 +1,26 @@
+# #1
+# name = input('What is your name?')
+# print(f'Hello, {name}')
+
+# #8 n_to_m
+# n= int(input('Start from: '))
+# m= int(input('End on: '))
+# count = n
+# while n<m:
+#     n+=1
+#     print(n)
+
+# #9 print a square:
+# count=0
+# s='*'*5
+# while count<5:
+#     count +=1
+#     print(s)
+
+#10 print a square2
+count =0
+n = int(input('How big is a square?'))
+s='*'* n
+while count<n:
+    count+=1
+    print(s)
