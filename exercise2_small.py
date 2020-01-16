7b13c707dabcc9f62d1a56fda6aec840d19b6b96
Alina Belova <belovaalni@gmail.com>
2020-01-16 11:29:04 -0600

    done

--- exercise2_small.py
+++ exercise2_small.py
@@ -26,3 +26,16 @@ list =[1,3,5,6,7,10,-4,7]
 #     if list[index]>0:
 #         positive_list.append(list[index])
 # print(positive_list)
+
+# #7
+# factor= 11
+# new_list=[]
+# for index in range(len(list)):
+#     multiplied_index= list[index]*factor
+#     new_list.append(multiplied_index)
+# print(new_list)
+
+# #8
+# string= "tuta larsen"
+# reversed_string=string[::-1]
+# print(reversed_string)
