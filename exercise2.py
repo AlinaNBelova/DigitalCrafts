bd079db0b7534d6d38034fafecec243480c6d220
Alina Belova <belovaalni@gmail.com>
2020-01-15 18:41:43 -0600

    wip

--- exercise2.py
+++ exercise2.py
@@ -19,3 +19,49 @@
 #     c.append(c_new)
 # print(c)
 
+# #3
+# a=[[1,3,5],[2,4,5],[3,4,6]]
+# b=[[5,22,2],[1,0,3],[1,2,4]]
+# c=[]
+# for i in range(len(a)):
+#     c_new=[]
+#     for j in range(len(a[i])):
+#         q = a[i][j] +b[i][j]
+#         c_new.append(q)
+#     c.append(c_new)
+# print(c)
+
+# #4
+# list =[1,2,7,8,5,9,6,3,7,5,]
+# list_sorted =list.sort()
+# print (list_sorted)
+# new_list =[]
+# for index in range(len(list_sorted)):
+#     new_list.append(list_sorted[index])
+#     if list_sorted[index]==list_sorted[index+1]:
+#         del list_sorted[index+1]
+#     print(new_list)
+
+#5
+string = "I am a leet programmer"
+print(string.upper())
+for index in range(len(string)):
+    if string[index]=='A':
+        string[index]="4"
+    if string[index]=='E':
+        string[index]='3'
+    if string[index]=='G':
+        string[index]='6'
+    if string[index]=='I':
+        string[index]='1'
+    if string[index]=='O':
+        string[index]='0'
+    if string[index]=='S':
+        string[index]='5'
+    if string[index]=='T':
+        string[index]='7'
+    print(string[index])
+
+
+
+
---
+++ exercise2_small.py
@@ -0,0 +1,28 @@
+list =[1,3,5,6,7,10,-4,7]
+# 1
+# print(sum(list))
+
+# #2
+# print(max(list))
+
+# #3
+# print(min(list))
+
+# #4
+# even_list =[]
+# for index in range(len(list)):
+#     if list[index]%2 ==0:
+#         even_list.append(list[index])
+# print(even_list)
+
+#5
+# for index in range(len(list)):
+#     if list[index]>0:
+#         print(list[index])
+
+# #6
+# positive_list =[]
+# for index in range(len(list)):
+#     if list[index]>0:
+#         positive_list.append(list[index])
+# print(positive_list)
